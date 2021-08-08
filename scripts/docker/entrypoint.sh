44ebc2b4c50c6c6895ee33e924ff594957c47057
jianlexia <56173042@qq.com>
2021-08-08 13:12:53 +0800

    add

--- scripts/docker/entrypoint.sh
+++ scripts/docker/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-#export BOOT1= /ip4/127.0.0.1/tcp/30333/p2p/12D3KooWBmAwcd4PJNJvfV89HwE48nwkRmAgo8Vy3uQEyNNHBox2
+export BOOT1= /ip4/45.32.147.48/tcp/30334/p2p/12D3KooWBmAwcd4PJNJvfV89HwE48nwkRmAgo8Vy3uQEyNNHBox2
 
 exec_gwichain(){
   exec node_template \
     --telemetry-url="wss://telemetry.polkadot.io/submit/ 0" \
-#    --bootnodes ${BOOT1} \
+    --bootnodes ${BOOT1} \
     --execution=NativeElseWasm \
     --rpc-methods=Unsafe \
     --unsafe-ws-external \
